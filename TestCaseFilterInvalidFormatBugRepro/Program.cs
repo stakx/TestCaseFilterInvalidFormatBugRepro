a26babe4fee44d57099c0040c4bd701adc0ca3f4
stakx <stakx@eml.cc>
2019-07-26 14:11:48 +0200

    Add demo workaround

--- TestCaseFilterInvalidFormatBugRepro/Program.cs
+++ TestCaseFilterInvalidFormatBugRepro/Program.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using NUnit.Framework;
 
 [TestFixture]
-public class Tests
+public class UnrunnableTests
 {
     [TestCaseSource("Source")]
     public void Test(Expression<Func<bool>> expression)
@@ -16,5 +16,33 @@ public class Tests
     {
         Expression<Func<bool>> expression = () => true;
         yield return expression;
+    }
+}
+
+[TestFixture]
+public class RunnableTests
+{
+    // The only difference to `UnrunnableTests` is that the test case source
+    // wraps the returned value in a `Opaque<T>` wrapper type.
+
+    [TestCaseSource("Source")]
+    public void Test(Opaque<Expression<Func<bool>>> wrappedExpression)
+    {
+    }
+
+    public static IEnumerable<Opaque<Expression<Func<bool>>>> Source()
+    {
+        Expression<Func<bool>> expression = () => true;
+        yield return new Opaque<Expression<Func<bool>>>(expression);
+    }
+
+    public readonly struct Opaque<T>
+    {
+        public readonly T Value;
+
+        public Opaque(T value)
+        {
+            this.Value = value;
+        }
     }
 }
