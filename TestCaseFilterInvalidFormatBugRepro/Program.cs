f0c0b6b7868973a7fa75372d46d93793f12a289a
stakx <stakx@eml.cc>
2019-07-26 13:59:29 +0200

    Add unit test that VS Test Explorer won't execute

---
+++ TestCaseFilterInvalidFormatBugRepro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class Tests
+{
+    [TestCaseSource("Source")]
+    public void Test(Expression<Func<bool>> expression)
+    {
+    }
+
+    public static IEnumerable<Expression<Func<bool>>> Source()
+    {
+        Expression<Func<bool>> expression = () => true;
+        yield return expression;
+    }
+}
